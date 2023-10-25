82770c696b4234a40eba5789c1c95f168225786a
Stéphane Bressani <stephane@bressani.dev>
2023-10-25 21:51:31 +0200

    initialisation de sdl

--- src/main.c
+++ src/main.c
@@ -2,6 +2,17 @@
 #include <SDL2/SDL.h>
 
 int main(int argc, char *argv[]) {
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window* window = SDL_CreateWindow(
+            "Chip8 Window",
+            SDL_WINDOWPOS_UNDEFINED,
+            SDL_WINDOWPOS_UNDEFINED,
+            640,
+            320,
+            SDL_WINDOW_SHOWN);
+
     printf("Hello, World!\n");
+
+    SDL_DestroyWindow(window);
     return 0;
 }
