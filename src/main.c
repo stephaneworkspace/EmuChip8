3cfc95166fec8c485c1864104fbe6784e466f970
Stéphane Bressani <stephane@bressani.dev>
2023-10-25 22:01:41 +0200

    le fenêtre affiche un carré

--- src/main.c
+++ src/main.c
@@ -11,8 +11,31 @@ int main(int argc, char *argv[]) {
             320,
             SDL_WINDOW_SHOWN);
 
-    printf("Hello, World!\n");
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_TEXTUREACCESS_TARGET);
 
+    int running = 1;
+    SDL_Event e;
+    while(running) {
+        while(SDL_PollEvent(&e)) {
+            if(e.type == SDL_QUIT) {
+                running = 0;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+        SDL_RenderClear(renderer);
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0);
+        SDL_Rect r;
+        r.x = 0;
+        r.y = 0;
+        r.w = 40;
+        r.h = 40;
+        SDL_RenderDrawRect(renderer, &r);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer); // Ajout de la destruction du rendu
     SDL_DestroyWindow(window);
+    SDL_Quit(); // Assurez-vous de quitter SDL proprement
     return 0;
 }
