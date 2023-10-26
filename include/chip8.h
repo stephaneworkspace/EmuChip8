0fabb9e7176a2031bf7a4118eaba214970492f3e
Stéphane Bressani <stephane@bressani.dev>
2023-10-26 17:38:30 +0200

    implémentation de la mémoire

--- include/chip8.h
+++ include/chip8.h
@@ -6,9 +6,10 @@
 #define EMUCHIP8_CHIP8_H
 
 #include "config.h"
+#include "chip8memory.h"
 
 struct chip8 {
-
+    struct chip8_memory memory;
 };
 
 #endif //EMUCHIP8_CHIP8_H
---
+++ include/chip8memory.h
@@ -0,0 +1,16 @@
+//
+// Created by Stéphane on 26.10.23.
+//
+
+#ifndef EMUCHIP8_CHIP8MEMORY_H
+#define EMUCHIP8_CHIP8MEMORY_H
+#include "config.h"
+
+struct chip8_memory {
+    unsigned char memory[CHIP8_MEMORY_SIZE];
+};
+
+void chip8_memory_set(struct chip8_memory* memory, int index, unsigned  char val);
+unsigned char chip8_memory_get(struct chip8_memory* memory, int index);
+
+#endif //EMUCHIP8_CHIP8MEMORY_H
